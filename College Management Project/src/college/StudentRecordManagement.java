6c23050079a37bb669070fb5b9b55002c0e1185b
Preetha Selvan <69691095+PreethaAyaana@users.noreply.github.com>
2023-12-18 21:05:16 +0530

    StudentRecordManagement.java

--- College Management Project/src/college/StudentRecordManagement.java
+++ College Management Project/src/college/StudentRecordManagement.java
@@ -1,38 +1,21 @@
-// Java Program to Illustrate StudentRecordManagement Class
-
 package college;
 
-// Importing required classes
 import java.util.LinkedList;
 import java.util.Scanner;
 
-// Class
 public class StudentRecordManagement {
 
-	// Creating an empty LinkedList
-	LinkedList<Record> list;
-
-	// Default Constructor
-	public StudentRecordManagement()
-	{
-		list = new LinkedList<>();
-	}
+	// Creating a LinkedList
+	LinkedList<Record> list = new LinkedList<>();
 
 	// Method 1
 	// Adding Record
-	// @param record
 	public void add(Record record)
 	{
-
-		// Checking if a record already exists or not,
-		// if not add it to Record list, Otherwise
-		// error display message
-		if (!find(record.getIdNumber())) {
+		if(!find(record.getIdNumber())) {
 			list.add(record);
 		}
-		else {
-
-			// Print statement
+		else{
 			System.out.println(
 				"Record already exists in the Record list");
 		}
@@ -40,17 +23,10 @@ public class StudentRecordManagement {
 
 	// Method 2
 	// Searching Record
-	// @param idNumber
-	// @return
 	public boolean find(int idNumber)
 	{
-
-		// Iterating record list
-		// using for each loop
-		for (Record l : list) {
-
-			// Checking record by id Number
-			if (l.getIdNumber() == idNumber) {
+		for(Record l : list) {
+			if(l.getIdNumber() == idNumber) {
 
 				System.out.println(l);
 				return true;
@@ -58,118 +34,72 @@ public class StudentRecordManagement {
 		}
 		return false;
 	}
-
+	
 	// Method 3
 	// Delete Record
-	// @param recIdNumber
 	public void delete(int recIdNumber)
 	{
 		Record recordDel = null;
-
-		// Iterating record list
-		for (Record ll : list) {
-
-			// Finding record to be deleted by id Number
-			if (ll.getIdNumber() == recIdNumber) {
+		
+		for(Record ll : list) {
+			if(ll.getIdNumber() == recIdNumber) {
 				recordDel = ll;
 			}
 		}
-
-		// If recordDel is null, then show error message,
-		// otherwise remove the record from Record list
-		if (recordDel == null) {
-
-			// Displaying no record found
+		if(recordDel == null) {
 			System.out.println("Invalid record Id");
 		}
-		else {
-
+		else{
 			list.remove(recordDel);
-
-			// Display message for successful deletion of
-			// record
 			System.out.println(
 				"Successfully removed record from the list");
 		}
 	}
-
+	
 	// Method 4
 	// Finding Record
-	// @param idNumber
-	// @return
 	public Record findRecord(int idNumber)
 	{
-
-		// Iterate Record list
-		// using for each loop
-		for (Record l : list) {
-
-			// Checking record by id Number.
-			if (l.getIdNumber() == idNumber) {
+		for(Record l : list) {
+			if(l.getIdNumber() == idNumber) {
 				return l;
 			}
 		}
-
 		return null;
 	}
-
+	
 	// Method 5
 	// Update Record
-	// @param id
-	// @param input
 	public void update(int id, Scanner input)
 	{
 
-		if (find(id)) {
+		if(find(id)) {
 			Record rec = findRecord(id);
-
-			// Display message only
-			System.out.print(
-				"What is the new Student id Number ? ");
+			System.out.print("Enter the new Student id Number ? ");
 			int idNumber = input.nextInt();
-
-			// Display message only
-			System.out.print(
-				"What is the new Student contact Number ");
+			System.out.print("Enter the new Student contact Number ");
 			int contactNumber = input.nextInt();
 			input.nextLine();
-
-			// Display message only
-			System.out.print(
-				"What is the new Student Name ? ");
+			System.out.print("Enter the new Student Name ? ");
 			String name = input.nextLine();
-
 			rec.setIdNumber(idNumber);
 			rec.setName(name);
 			rec.setContactNumber(contactNumber);
-			System.out.println(
-				"Record Updated Successfully");
+			System.out.println("Record Updated Successfully");
 		}
-		else {
-
-			// Print statement
-			System.out.println(
-				"Record Not Found in the Student list");
+		else{
+			System.out.println("Record Not Found in the Student list");
 		}
 	}
-
+	
 	// Method 6
 	// Display Records
 	public void display()
 	{
-
-		// If record list is empty then
-		// print the message below
-		if (list.isEmpty()) {
-
-			// Print statement
-			System.out.println("The list has no records\n");
+		if(list.isEmpty()) {
+			System.out.println("The list has no records");
 		}
-		// Iterating Record list
-		// using for each loop
-		for (Record record : list) {
-
-			// Printing the list
+		for(Record record : list) {
 			System.out.println(record.toString());
 		}
 	}
